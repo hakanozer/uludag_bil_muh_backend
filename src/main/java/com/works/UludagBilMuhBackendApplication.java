fa209f1f6247ee49c78cce34633479c5600dd92a
Hakan ÖZER <hakanozer02@gmail.com>
2026-03-23 11:08:51 +0300

    Project Start

---
+++ src/main/java/com/works/UludagBilMuhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UludagBilMuhBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UludagBilMuhBackendApplication.class, args);
+    }
+
+}
